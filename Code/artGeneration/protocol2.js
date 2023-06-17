130e77be81287c1252d9938a555502d59b4d9e95
jack.hollingsworth <jackhhollings@gmail.com>
2023-06-17 00:46:20 -0400

    protocol 2 updates

--- Code/artGeneration/protocol2.js
+++ Code/artGeneration/protocol2.js
@@ -6,42 +6,28 @@ function generateArt() {
     const shiftX = parseInt(document.getElementById("shiftX").value);
     const shiftY = parseInt(document.getElementById("shiftY").value);
     const ellipseWidth = parseInt(document.getElementById("ellipseWidth").value);
-    const ellipseHeight = parseInt(
-      document.getElementById("ellipseHeight").value
-    );
+    const ellipseHeight = parseInt(document.getElementById("ellipseHeight").value);
+    const ellipseSpacing = parseInt(document.getElementById("ellipseSpacing").value);
   
     const svgContainer = document.getElementById("svgContainer");
     svgContainer.innerHTML = "";
   
     const width = svgContainer.clientWidth;
     const height = svgContainer.clientHeight;
-    const points = [];
   
-    for (let x = 0; x <= width; x += 5) {
-      const radian = (x * frequency + phase) * (Math.PI / 180);
+    const points = [];
+    for (let x = 0; x <= width; x += ellipseSpacing) {
+      const radian = ((x + shiftX) * frequency + phase) * (Math.PI / 180);
       const y =
         height / 2 -
         amplitude * Math.pow(Math.sin(radian), degree / 2) +
         shiftY;
-      points.push(`${x + shiftX},${y}`);
+      points.push({ x, y });
     }
   
-    const polyline = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "polyline"
-    );
-    polyline.setAttribute("points", points.join(" "));
-    polyline.setAttribute("fill", "none");
-    polyline.setAttribute("stroke", "black");
-    polyline.setAttribute("stroke-width", "2");
-    svgContainer.appendChild(polyline);
-  
     for (let i = 0; i < points.length; i++) {
-      const [x, y] = points[i].split(",");
-      const ellipse = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "ellipse"
-      );
+      const { x, y } = points[i];
+      const ellipse = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
       ellipse.setAttribute("cx", x);
       ellipse.setAttribute("cy", y);
       ellipse.setAttribute("rx", ellipseWidth);
