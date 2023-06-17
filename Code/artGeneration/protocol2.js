852403c6f67258e8d6536a7d2b9238d7c7367347
jack.hollingsworth <jackhhollings@gmail.com>
2023-06-17 00:51:59 -0400

    added useless slider

--- Code/artGeneration/protocol2.js
+++ Code/artGeneration/protocol2.js
@@ -7,7 +7,8 @@ function generateArt() {
     const shiftY = parseInt(document.getElementById("shiftY").value);
     const ellipseWidth = parseInt(document.getElementById("ellipseWidth").value);
     const ellipseHeight = parseInt(document.getElementById("ellipseHeight").value);
-    const ellipseSpacing = parseInt(document.getElementById("ellipseSpacing").value);
+    const ellipseSpacing = parseFloat(document.getElementById("ellipseSpacing").value);
+    const ellipseRotation = parseInt(document.getElementById("ellipseRotation").value);
   
     const svgContainer = document.getElementById("svgContainer");
     svgContainer.innerHTML = "";
@@ -17,38 +18,25 @@ function generateArt() {
   
     const points = [];
     for (let x = 0; x <= width; x += ellipseSpacing) {
-      const radian = ((x + shiftX) * frequency + phase) * (Math.PI / 180);
-      const y =
-        height / 2 -
-        amplitude * Math.pow(Math.sin(radian), degree / 2) +
-        shiftY;
-      points.push({ x, y });
+        const radian = ((x + shiftX) * frequency + phase) * (Math.PI / 180);
+        const y =
+            height / 2 -
+            amplitude * Math.pow(Math.sin(radian), degree / 2) +
+            shiftY;
+        points.push({ x, y });
     }
   
     for (let i = 0; i < points.length; i++) {
-      const { x, y } = points[i];
-      const ellipse = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
-      ellipse.setAttribute("cx", x);
-      ellipse.setAttribute("cy", y);
-      ellipse.setAttribute("rx", ellipseWidth);
-      ellipse.setAttribute("ry", ellipseHeight);
-      ellipse.setAttribute("fill", "none");
-      ellipse.setAttribute("stroke", "black");
-      ellipse.setAttribute("stroke-width", "2");
-      svgContainer.appendChild(ellipse);
+        const { x, y } = points[i];
+        const ellipse = document.createElementNS("http://www.w3.org/2000/svg", "ellipse");
+        ellipse.setAttribute("cx", x);
+        ellipse.setAttribute("cy", y);
+        ellipse.setAttribute("rx", ellipseWidth);
+        ellipse.setAttribute("ry", ellipseHeight);
+        ellipse.setAttribute("fill", "none");
+        ellipse.setAttribute("stroke", "black");
+        ellipse.setAttribute("stroke-width", "2");
+        ellipse.setAttribute("transform", `rotate(${ellipseRotation}, ${x}, ${y})`);
+        svgContainer.appendChild(ellipse);
     }
-  }
-  
-  // Attach event listeners to the sliders
-  const sliders = document.querySelectorAll('input[type="range"]');
-  sliders.forEach((slider) => {
-    slider.addEventListener("input", () => {
-      const valueSpan = document.getElementById(`${slider.id}Value`);
-      valueSpan.textContent = slider.value;
-      generateArt();
-    });
-  });
-  
-  // Generate initial art
-  generateArt();
-  
+}
